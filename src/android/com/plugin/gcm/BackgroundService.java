8a28ce28df62ab94cd6a39dd1e75f4f27ac4fc4f
= <fernando@soluciones3f.com.ar>
2015-02-12 12:10:47 -0300

    Llamamos a una URL Hardcoded para informar de la acción. Leemos el response de esa URL :)

--- src/android/com/plugin/gcm/BackgroundService.java
+++ src/android/com/plugin/gcm/BackgroundService.java
@@ -6,6 +6,11 @@ import android.content.Intent;
 import android.content.Context;
 import android.util.Log;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.io.IOException;
+
 public class BackgroundService extends IntentService {
     private static final String TAG = "BackgroundService";
 
@@ -18,6 +23,25 @@ public class BackgroundService extends IntentService {
         Log.v(TAG, "Intent Handled");
 
         final NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-    	notificationManager.cancelAll();
+        notificationManager.cancelAll();
+
+        try {
+            URL url = new URL("http://192.168.1.6:8080/nire/process/processCommand?token=1");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                int responseCode = urlConnection.getResponseCode();
+                Log.v(TAG, "Response code: " + responseCode);
+            }
+            finally {
+                urlConnection.disconnect();
+            }
+        }
+        catch(MalformedURLException e) {
+            Log.v(TAG, "Exception. Malformed URL");
+        }
+        catch(IOException e) {
+            Log.v(TAG, "Exception. IOException");
+        }
+
     }
 }
